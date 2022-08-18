309081ec58dad9fc85f4e134c02eb3aee50083eb
cecialbero <ceci.albero@gmail.com>
2022-08-18 17:46:22 -0300

    Add context into Welcome comp

--- client/components/Welcome.tsx
+++ client/components/Welcome.tsx
@@ -1,18 +1,25 @@
+import { useContext, SyntheticEvent } from 'react'
 import { AiFillPlayCircle } from 'react-icons/ai'
 import { SiEthereum } from 'react-icons/si'
 import { BsInfoCircle } from 'react-icons/bs'
 
+import { TransactionContext } from '../context/TransactionContext'
 import { Loader, Input } from './'
 
 const companyCommonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
 const Welcome = () => {
-    const connectWallet = () => {};
-    const handleChange = () => {};
+    const { connectWallet, currentAccount, formData, handleChange, sendTransaction } = useContext(TransactionContext)
 
-    const handleSubmit = (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault();
-    };
+    const handleSubmit = (e: SyntheticEvent) => {
+        const { addressTo, amount, keyword, message } = formData
+
+        e.preventDefault()
+
+        if(!addressTo || !amount || !keyword || !message) return
+
+        sendTransaction()
+    }
 
     return (
         <div className='flex w-full justify-center items-center'>
@@ -24,16 +31,19 @@ const Welcome = () => {
                     <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
                         Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
                     </p>
-                    <button
-                        type='button'
-                        onClick={connectWallet}
-                        className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
-                    >
-                        <AiFillPlayCircle className='text-white mr-2' />
-                        <p className='text-white text-base font-semibold'>
-                            Connect Wallet
-                        </p>
-                    </button>
+                    
+                    {!currentAccount && (
+                        <button
+                            type='button'
+                            onClick={connectWallet}
+                            className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
+                        >
+                            <AiFillPlayCircle className='text-white mr-2' />
+                            <p className='text-white text-base font-semibold'>
+                                Connect Wallet
+                            </p>
+                        </button>
+                    )}
 
                     <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
                         <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
